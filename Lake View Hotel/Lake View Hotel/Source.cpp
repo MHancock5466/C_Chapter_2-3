70322c0c0fb0c7be71ef02963a4e2b9f55260ce1
Mason <mhancock5466@gmail.com>
2018-09-04 10:49:40 -0500

    Initial Lake View Hotel Code

--- Lake View Hotel/Lake View Hotel/Source.cpp
+++ Lake View Hotel/Lake View Hotel/Source.cpp
@@ -7,10 +7,23 @@ using namespace std;
 
 double findCost(int days, int minutes)
 {
+	double dayRate = 100;
+	double minuteRate = 0.25;
 	double cost = days * dayRate + minutes * minuteRate;
+	return cost;
 }
 
 int main()
 {
+	int days;
+	int minutes;
 
+	cout << "How many days have you stayed? ";
+	cin >> days;
+	cout << "How many minutes were you on the phone? ";
+	cin >> minutes;
+	cout << "Your bill will be: $" << findCost(days, minutes) << endl;
+
+	system("pause");
+	return 0;
 }
