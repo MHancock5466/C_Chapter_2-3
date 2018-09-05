c0953ca2c07c7edf6ab695e22c0f9c93e791715b
Mason <mhancock5466@gmail.com>
2018-09-05 10:20:34 -0500

    Update

--- Savings Account/Savings Account/Source.cpp
+++ Savings Account/Savings Account/Source.cpp
@@ -7,28 +7,33 @@ using namespace std;
 
 double savings(double earnings)
 {
-	double percentToSavings = 1;
+	double percentToSavings = .1;
 	double checkSavings = earnings * percentToSavings;
 	return checkSavings;
 }
 
-double totalSavings(double earnings)
-{
-	double totalSavings;
-	totalSavings = totalSavings + savings(earnings);
-	return totalSavings;
-}
-
 int main()
 {
+	double earnings = 0;
+	double totalSavings = 0;
+	double bonus = 0;
+
 	for (int checkNumber = 1; checkNumber <= 24; checkNumber++)
 	{
-		double earnings = 0;
-
 		cout << "What was your paycheck amount for check #" << checkNumber << "? ";
-		cin >> earnings;
-		totalSavings(earnings);
+		cin >> earnings;	
+		totalSavings = totalSavings + savings(earnings);
 	}
+
+	cout << "How much did you receive from your bonus check? ";
+	cin >> bonus;
+
+	if (bonus >= 100)
+		totalSavings = totalSavings + 100;
+	else
+		totalSavings = totalSavings + bonus;
+
+	cout << "Your Savings This Year Totals: $" << totalSavings << endl;
 
 	system("pause");
 	return 0;
