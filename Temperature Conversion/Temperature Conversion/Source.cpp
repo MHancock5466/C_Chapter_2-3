378d108695de41098eeb2b8353c04423c2acaa25
Mason <mhancock5466@gmail.com>
2018-09-28 10:28:57 -0500

    Update

--- Temperature Conversion/Temperature Conversion/Source.cpp
+++ Temperature Conversion/Temperature Conversion/Source.cpp
@@ -1,46 +1,24 @@
 //Converting Fahrenheit to Celcius
 //Author: Mason Hancock
-//Date: 08/30/2018
-
+//Date: 08/30/2018.
 #include <iostream>
-#include <string>
+#include <iomanip>
 using namespace std;
 
-double convert(double temp)
-{
+double convert(double temp) {
 	double output = (temp - 32) * 5 / 9;
 	return output;
 }
 
-int main()
-{
-	double temp1 = 0;
-	double temp2 = 0;
-	double temp3 = 0;
-
-	cout << "Input the temperature at 8:00 am: ";
-	cin >> temp1;
-	cout << "Input the temperature at 12:00 pm: ";
-	cin >> temp2;
-
-	while(temp2 < temp1)
-	{
-		cout << "Error: Temperature must not be smaller than the previous temperature.\nInput the temperature at 12:00 pm: ";
-		cin >> temp2;
+int main() {
+	double tempArray[5] = { 0.0 };
+	//for loop
+	//intialize array to hold temp values
+	for (int i = 0; i < size(tempArray); i++) {
+		cout << "Enter a temperature >> ";
+		cin >> tempArray[i];
+		cout << fixed << setprecision(2) << "The temperature conversion is " << convert(tempArray[i]) << " degrees Celsius." << endl;
 	}
-
-	cout << "Input the temperature at 5:00 pm: ";
-	cin >> temp3;
-
-	while (temp3 > 10 + temp2)
-	{
-		cout << "Error: Temperature must not exceed previous temperature by 10 degrees.\nInput the temperature at 5:00 pm: ";
-		cin >> temp3;
-	} 
-
-	cout << "Temperature at 8:00 am: 		" << temp1 << " degrees F		" << convert(temp1) << " degrees C" << endl;
-	cout << "Temperature at 12:00 pm:		" << temp2 << " degrees F		" << convert(temp2) << " degrees C" << endl;
-	cout << "Temperature at 5:00 pm: 		" << temp3 << " degrees F		" << convert(temp3) << " degrees C" << endl;
 
 	system("pause");
 	return 0;
